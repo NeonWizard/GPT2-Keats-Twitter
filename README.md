d787880dff9cdc926b42f0385083a672405a0091
Spooky <averagewizard13@gmail.com>
2020-09-23 14:20:45 -0600

    Add systemd stuff and install script

--- README.md
+++ README.md
@@ -18,4 +18,11 @@ I uploaded my master training text file to the Collab instance and downloaded th
 
 ## Coding the Twitter bot
 
-## Integrating with Heroku
+## Creating the systemd service and timer
+
+-----
+
+## Installation Instructions
+1. Clone the repo into your system, then `cd` into it.
+2. Ensure the proper GPT2 models are on your system, and then symlink the directory with `ln -s <path_to_models> models`.
+3. Run `sudo bash install.sh` to install pip packages, and set up the systemd service and timer.
---
+++ install.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# install pip requirements
+sudo python3.7 -m pip install -r requirements.txt
+
+# install systemd service and timer
+if [ -f /etc/systemd/system/keats-twitterbot.timer ]; then
+    sudo systemctl disable keats-twitterbot.timer
+    sudo systemctl stop keats-twitterbot.timer
+fi
+
+eval "echo -e \"`<keats-twitterbot.service`\"" > /etc/systemd/system/keats-twitterbot.service
+eval "echo -e \"`<keats-twitterbot.timer`\"" > /etc/systemd/system/keats-twitterbot.timer
+
+sudo systemctl daemon-reload
+sudo systemctl enable keats-twitterbot.timer
+sudo systemctl start keats-twitterbot.timer
